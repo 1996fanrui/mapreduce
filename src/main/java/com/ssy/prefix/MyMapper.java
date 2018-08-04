3c5e8d9cb554bae349905667af105a1f19f137eb
fanrui <fanrui@weli.cn>
2018-08-04 09:24:17 +0800

    1、完成了Combiner 2、后缀的完成了分后缀统计

--- src/main/java/com/ssy/prefix/MyMapper.java
+++ src/main/java/com/ssy/prefix/MyMapper.java
@@ -11,11 +11,16 @@ public class MyMapper extends Mapper<Object, Text, Text, IntWritable> {
 
     public static final IntWritable one = new IntWritable(1);
     private Text appKey = new Text();
+    private String appStr;
 
     public void map(Object key, Text value, Context context)
             throws IOException, InterruptedException {
-        appKey.set( value.toString().split(",")[1] );
+        appStr = value.toString().split(",")[1];
+        if( "91988061".equals(appStr) || "99817749".equals(appStr) ){
+            appKey.set( value.toString().split(",")[1] + (int)(10*Math.random()) );
+        } else{
+            appKey.set( value.toString().split(",")[1] );
+        }
         context.write(appKey,one);
     }
-
 }
---
+++ src/main/java/com/ssy/prefix/WC.java
@@ -0,0 +1,56 @@
+package com.ssy.prefix;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class WC {
+
+    public static void main(String[] args)
+            throws IOException, ClassNotFoundException, InterruptedException {
+
+        Configuration conf = new Configuration(true);//读取配置文件
+        conf.set("mapred.jar", "E:\\code\\ssy\\mapreduce\\out\\artifacts\\MyWC\\MyWC.jar");
+        conf.set("fs.defaultFS", "hdfs://nn1.hadoop.bigdata.dmp.com:8020");
+//        conf.set(FileInputFormat.SPLIT_MINSIZE,"67108464");   //64M
+//        conf.set(FileInputFormat.SPLIT_MINSIZE,"268433856"); //256M
+        System.setProperty("HADOOP_USER_NAME", "root");
+
+        Job  job = Job.getInstance(conf); //job  作业
+        job.setJarByClass(WC.class);//参数为 当前类的 类名
+        job.setJobName("prefix");//给当前Job 取个名字
+        job.setNumReduceTasks(24);
+
+        Path input = new Path("/user/hive/warehouse/test.db/tmp_ods_hive_pv_1d");//输入路径
+        RemoteIterator<LocatedFileStatus> remoteIterator =
+                input.getFileSystem(conf).listFiles(input,false);
+        int inputCount = 0;
+        while ( remoteIterator.hasNext() ){
+            LocatedFileStatus locatedFileStatus = remoteIterator.next();
+            FileInputFormat.addInputPath( job, locatedFileStatus.getPath() );//为job添加输入路径
+            inputCount++;
+        }
+        System.out.println( "数据源总共" + inputCount + "个文件！！！" );
+        Path output = new Path("/tmp/prefixWC");   //输出路径
+        if( output.getFileSystem(conf).exists(output) ) {       //如果路径存在,删除路径
+            output.getFileSystem(conf).delete(output,true);
+        }
+        FileOutputFormat.setOutputPath( job, output );//为job设置输出路径
+
+        job.setMapperClass(MyMapper.class);    //设置map的类
+        job.setMapOutputKeyClass(Text.class);    //map的key输出类型
+        job.setMapOutputValueClass(IntWritable.class);//map的value输出类型
+        job.setReducerClass(MyReducer.class);    //设置reduce的类
+        //将以上所有的代码 提交给集群,等待 完成
+
+        job.waitForCompletion(true);
+    }
+}
